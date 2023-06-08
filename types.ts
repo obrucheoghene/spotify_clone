aea60ba21449bd8602f672082b23e4d3a5050bef
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-08 08:12:16 +0100

    fix: Fix product type

--- types.ts
+++ types.ts
@@ -32,7 +32,7 @@ export interface Price {
     product_id: string;
     active?: boolean;
     description?: string;
-    unit_amount: number;
+    unit_amount: number |undefined;
     currency: string;
     type?: Stripe.Price.Type;
     interval?: Stripe.Price.Recurring.Interval;
