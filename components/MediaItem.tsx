2bcb1c52bf4a081a5c352a5632eb5562ff587007
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-08 11:09:32 +0100

    fix: Handle player

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types"
 import Image from "next/image";
 
@@ -10,12 +11,13 @@ interface MediaItemProps {
 }
 const MediaItem: React.FC<MediaItemProps> = ({data, onClick}) => {
     const imageUrl = useLoadImage(data)
+    const player = usePlayer();
 
     const handleClick = () => {
         if (onClick) {
             return onClick(data.id)
         }
-        // Turn on player
+        player.setId(data.id)
     }
   return (
     <div
